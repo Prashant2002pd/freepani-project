29d8f3711df630e3cc53792abb704b5188fbe91e
= <mridultiwari2002@gmail.com>
2024-09-11 08:50:31 +0530

    fixed setup errors in Next js

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+// import "@styles/global.css";
 import Nav from "@components/Nav";
-import Provider from "@components/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,12 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Provider>
           <main>
             <Nav />
             {children}
           </main>
-        </Provider>
       </body>
     </html>
   );
