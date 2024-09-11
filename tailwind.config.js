eea82b5da80563f92e5132800f7c9fbc39de388e
Prashant2002pd <92350732+Prashant2002pd@users.noreply.github.com>
2024-09-11 17:41:34 +0530

    some updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const withMT = require("@material-tailwind/react/utils/withMT");
- 
+
 module.exports = withMT({
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -18,13 +18,13 @@ module.exports = withMT({
           black: "#000000",
           green: "#1dba1c",
           white: "#ffffff",
-          darkblue: "0A0F22",
-          lightblue: "56B2BB",
-          grey: "BAC7CC",
+          darkblue: "#0A0F22",
+          lightblue: "#56B2BB",
+          grey: "#BAC7CC",
         },
         secondary: {
-          darkblue: "1d2233",
-          grey: "f0f4f8",
+          darkblue: "#1d2233",
+          grey: "#f0f4f8",
         },
       },
       backgroundColor: {
@@ -32,13 +32,13 @@ module.exports = withMT({
           black: "#000000",
           green: "#1dba1c",
           white: "#ffffff",
-          darkblue: "0A0F22",
-          lightblue: "56B2BB",
-          grey: "BAC7CC",
+          darkblue: "#0A0F22",
+          lightblue: "#56B2BB",
+          grey: "#BAC7CC",
         },
         secondary: {
-          darkblue: "1d2233",
-          grey: "f0f4f8",
+          darkblue: "#1d2233",
+          grey: "#f0f4f8",
         },
       },
     },
