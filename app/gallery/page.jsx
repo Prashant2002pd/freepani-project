db2aae709f70aaee5f70db58304fcb3ec266d802
= <mridultiwari2002@gmail.com>
2024-09-18 21:12:21 +0530

    added bento grid to gallery

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -6,7 +6,8 @@ import { FaHome } from "react-icons/fa";
 const Gallery = () => {
   return (
     <Template>
-      <div className="mb-96 pt-20 pb-10 bg-primary-black grid place-items-start px-32">
+      <div className="mb-96 pt-20 pb-10 bg-primary-black grid place-items-start px-32 pb-24">
+        <div className="px-52">
         <Breadcrumbs
           separator="|"
           className="rounded-full bg-gray-600 text-primary-white py-1 px-4 mb-4"
@@ -15,7 +16,7 @@ const Gallery = () => {
             href="/"
             className="text-primary-white opacity-60 hover:opacity-100"
           >
-            <FaHome/>
+            <FaHome />
           </Link>
           <Link
             href="/contact_us"
@@ -24,7 +25,19 @@ const Gallery = () => {
             Contact Us
           </Link>
         </Breadcrumbs>
-        <img src="https://i.pinimg.com/564x/96/c6/3e/96c63e77096f8af8964cfcbb47c3df23.jpg" className="w-full" />
+        </div>
+        <div className="w-full grid place-items-center">
+          <div className="grid auto-rows-[400px] w-9/12 grid-cols-3 gap-4">
+            {[...Array(15)].map((_, i) => (
+              <div
+                key={i}
+                className={`row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-100 p-4 dark:bg-neutral-900 ${i === 3 || i === 6 ||i==10 ? "col-span-2" : ""
+                  }`}
+              ></div>
+            ))}
+          </div>
+        </div>
+        
       </div>
     </Template>
   );
